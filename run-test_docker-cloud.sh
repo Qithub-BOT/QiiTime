352f677d17537ca294e5b8a9d95d78c77d842077
KEINOS <github@keinos.com>
2020-01-03 02:11:41 +0900

    docs: コメント文の見直し

--- run-test_docker-cloud.sh
+++ run-test_docker-cloud.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-
-# This script is a test for automated build of Docker Cloud.
+# This script is a test for the automated build of Docker Cloud.
 #
-# Mount this file and replace the entrypoint script to run
-# from run-server.sh to run-test.sh
+# Mount this file to /app/run-test.sh as "entrypoint".
 
 echo '- Running test ...'
 
@@ -12,8 +10,7 @@ name_host=${name_host:-$(cat /etc/hostname)}
 
 echo "- Host name: ${name_host}"
 echo '- Installing curl ...'
-apk update && \
-apk add curl && \
+apk --no-cache add curl && \
 echo 'Installed at:' $(which curl) && \
 echo '- Running server ...'
 nohup /app/run-server.sh &
